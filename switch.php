b7c3182bef2e0096567b110cf93deea8dcdf9bad
tomoki1230 <tomoki_1230@yahoo.co.jp>
2021-09-26 21:54:46 +0900

    switch文fruits例を追加

---
+++ switch.php
@@ -0,0 +1,22 @@
+<?php
+
+$fruits = "apple";
+
+switch($fruits) {
+    case "banana":
+        echo "バナナです";
+        break;
+    case "apple":
+        echo "りんごです";
+        break;
+    case "orange":
+        echo "オレンジです";
+        break;
+    case "mango":
+        echo "マンゴーです";
+        break;
+    default:
+        echo "不正な名前です";
+}
+
+?>
