7609e17195240847671845d2f13dab223592b85d
tomoki1230 <tomoki_1230@yahoo.co.jp>
2021-09-26 22:07:56 +0900

    switch文month例を追加

--- switch.php
+++ switch.php
@@ -20,3 +20,34 @@ switch($fruits) {
 }
 
 ?>
+
+<?php
+
+$month = 6;
+
+switch($month) {
+    case 3:
+    case 4:
+    case 5:
+        echo "春です";
+        break;
+    case 6:
+    case 7:
+    case 8:
+        echo "夏です";
+        break;
+    case 9:
+    case 10:
+    case 11:
+        echo "秋です";
+        break;
+    case 12:
+    case 1:
+    case 2:
+        echo "冬です";
+        break;
+    default:
+        echo "不正な値です";
+}
+
+?>
