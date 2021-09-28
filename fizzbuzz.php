7c4332d3092da09210bf50e1e4ca869673978e39
tomoki1230 <tomoki_1230@yahoo.co.jp>
2021-09-28 23:10:59 +0900

    FizzBuzzを実装

---
+++ fizzbuzz.php
@@ -0,0 +1,29 @@
+<?php
+
+for($i=1 ; $i < 101 ; $i++){
+
+    if($i % 3 == 0 && $i % 5 == 0){
+
+        echo "FizzBuzz";
+        echo "<br>";
+
+    }elseif($i % 3 == 0){
+
+        echo "Fizz";
+        echo "<br>";
+
+    }elseif($i % 5 == 0){
+
+        echo "Buzz";
+        echo "<br>";
+
+    }else{
+
+        echo $i;
+        echo "<br>";
+
+    }
+
+}
+
+?>
